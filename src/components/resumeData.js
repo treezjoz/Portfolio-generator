0001f060dd37c1f46bc004ed7a04cf01532a6cbf
Treesa Jose <treesajisjoy3280@gmail.com>
2021-07-21 16:45:55 +0530

    sample data

---
+++ src/components/resumeData.js
@@ -0,0 +1,82 @@
+//DEMO resumeData
+let resumeData = {
+    "firstName":"Treesa",
+    "lastName":" Jose",
+    "role1": "Frontend Developer",
+    "role2":"Designer",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies or simply watch movies/series in my free time.",
+    "aboutme":"I am currently a pre-final year student at Mar Athanasius College Of Engineering Kothamangalam and pursuing my B.Tech from here. I am a self taught Web Developer. ",
+    "photo":"images/xabout.jpg",
+    "location":"Kerala",
+    "zipcode":"688531",
+    "email":"treesajisjoy3280@gmail.com",
+    "phone":"890XXXXXXX",
+    "linkedin":"https://www.linkedin.com/in/treesa-jose/",
+    "github":"https://github.com/treezjoz",
+    "instagram":"https://www.instagram.com/treez_joz/",
+    "education":[
+      {
+        "UniversityName":"Kerala Technological University",
+        "specialization":"B.tech Computer SCience",
+        "TermOfStudy":"2018-22",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Kerala Technological University",
+        "specialization":"B.tech Computer SCience",
+        "TermOfStudy":"2018-22",
+        "Achievements":"Some Achievements"
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Company",
+        "specialization":"specialization",
+        "Term":"2022-23",
+        "Achievements":"Some Achievements"
+      },
+    ],
+    "skills":[
+      {
+        "skillname":"HTML5",
+        "percent":"50"
+      },
+      {
+        "skillname":"Django",
+        "percent":"50"
+      },
+      {
+        "skillname":"Reactjs",
+        "percent":"50"
+      }
+    ],
+    "achievements":[
+      {
+        "year":"2021",
+        "award":"ghc scholar",
+        "authority":"AnitaB.org"
+      },
+    ],
+    "projects":[
+      {
+        "name":"project1",
+        "description":"webapp",
+        "imgurl":'images/work2.jpg',
+        "link":"#"
+      },
+      {
+        "name":"project2",
+        "description":"webapp",
+        "imgurl":'images/work2.jpg',
+        "link":"#"
+      },
+      {
+        "name":"project3",
+        "description":"webapp",
+        "imgurl":'images/work2.jpg',
+        "link":"#"
+      },
+    ]
+  }
+
+  export default resumeData;
