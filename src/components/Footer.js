c74fe6b2c3a519625b00d28e0806aa65f8e2b714
Treesa Jose <treesajisjoy3280@gmail.com>
2021-07-22 17:05:44 +0530

    linkedin not mandatory

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ export default function Footer(props){
             <div className="ftco-footer-widget" style={{textAlign:'center'}}>
               <h2 className="ftco-heading-2">Wanna connect with me?!</h2>
               <ul className="ftco-footer-social list-unstyled">
-                <li ><a href={props.resumeData.linkedin} target="_blank" rel="noopener noreferrer"><span className="fa fa-linkedin"></span></a></li>
+                { props.resumeData.linkedin && <li ><a href={props.resumeData.linkedin} target="_blank" rel="noopener noreferrer"><span className="fa fa-linkedin"></span></a></li> }
                 { props.resumeData.github && <li ><a href={props.resumeData.github} target="_blank" rel="noopener noreferrer"><span className="fa fa-github"></span></a></li> }
                 { props.resumeData.instagram && <li ><a href={props.resumeData.instagram} target="_blank" rel="noopener noreferrer"><span className="fa fa-instagram"></span></a></li> }
               </ul>
