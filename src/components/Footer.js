a5d7368f9b5ff96777f6488802d18f352578b579
Treesa Jose <treesajisjoy3280@gmail.com>
2021-07-22 08:51:14 +0530

    contact links optional

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,8 +10,8 @@ export default function Footer(props){
               <h2 className="ftco-heading-2">Wanna connect with me?!</h2>
               <ul className="ftco-footer-social list-unstyled">
                 <li ><a href={props.resumeData.linkedin} target="_blank" rel="noopener noreferrer"><span className="fa fa-linkedin"></span></a></li>
-                <li ><a href={props.resumeData.github} target="_blank" rel="noopener noreferrer"><span className="fa fa-github"></span></a></li>
-                <li ><a href={props.resumeData.instagram} target="_blank" rel="noopener noreferrer"><span className="fa fa-instagram"></span></a></li>
+                { props.resumeData.github && <li ><a href={props.resumeData.github} target="_blank" rel="noopener noreferrer"><span className="fa fa-github"></span></a></li> }
+                { props.resumeData.instagram && <li ><a href={props.resumeData.instagram} target="_blank" rel="noopener noreferrer"><span className="fa fa-instagram"></span></a></li> }
               </ul>
             </div>
           </div>
